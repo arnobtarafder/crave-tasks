20030eca63372b76a742c47656e72f8cac0f7cd8
arnobtarafder <rmtarnob@gmail.com>
2022-07-01 04:52:16 +0600

    add: navigation bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Navbar from './Components/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
+
+      <Navbar />
       
       <h1>Hello Crave</h1>
+      <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
 
     </div>
   );
