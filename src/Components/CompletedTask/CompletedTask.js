d555b365e811cc248c09cdb95879e34938c0edf7
arnobtarafder <rmtarnob@gmail.com>
2022-07-01 18:56:23 +0600

    style: app file

--- src/Components/CompletedTask/CompletedTask.js
+++ src/Components/CompletedTask/CompletedTask.js
@@ -11,7 +11,7 @@ const CompletedTask = () => {
     refetch,
   } = useQuery("completed", () =>
     fetch(
-      `http://localhost:5000/my-tasks/completed?email=${auth?.currentUser?.email}`,
+      `http://localhost:5000/myTasks/completed?email=${auth?.currentUser?.email}`,
       {
         headers: {
           authorization: `Bearer ${localStorage.getItem("accessToken")}`,
@@ -49,9 +49,9 @@ const CompletedTask = () => {
                     viewBox="0 0 24 24"
                   >
                     <path
-                      strokeLinecap="round"
-                      strokeLinejoin="round"
-                      strokeWidth="2"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
                       d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
                     />
                   </svg>
